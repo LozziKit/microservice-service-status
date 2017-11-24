7b8cd612fa3f76d88f8272ea9690f898b3bf71cc
Oceanos1 <michael.spierer@heig-vd.ch>
2017-11-24 15:45:02 +0100

    mise en page avant merge sur master

--- service-status-specs/src/test/java/io/lozzikit/servicestatus/api/spec/steps/utils/HttpSteps.java
+++ service-status-specs/src/test/java/io/lozzikit/servicestatus/api/spec/steps/utils/HttpSteps.java
@@ -17,8 +17,6 @@ public class HttpSteps {
     private Environment environment;
     private ServiceApi api;
 
-
-
     public HttpSteps(Environment environment) {
         this.environment = environment;
         this.api = environment.getApi();
