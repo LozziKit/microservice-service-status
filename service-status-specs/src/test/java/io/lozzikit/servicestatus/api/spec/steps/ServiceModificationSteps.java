cdbb4845f2f39812d9d7c16be38bcca7f5646d74
Edward Ransome <edward.ransome@heig-vd.ch>
2017-11-24 15:36:40 +0100

    before pull

--- service-status-specs/src/test/java/io/lozzikit/servicestatus/api/spec/steps/ServiceModificationSteps.java
+++ service-status-specs/src/test/java/io/lozzikit/servicestatus/api/spec/steps/ServiceModificationSteps.java
@@ -1,11 +1,14 @@
 package io.lozzikit.servicestatus.api.spec.steps;
 
+import com.google.gson.Gson;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
+import io.lozzikit.servicestatus.api.dto.ApiError;
 import io.lozzikit.servicestatus.api.dto.NewService;
 import io.lozzikit.servicestatus.api.spec.helpers.Environment;
 import io.lozzkit.servicestatus.ApiException;
+import io.lozzkit.servicestatus.ApiResponse;
 import io.lozzkit.servicestatus.api.ServiceApi;
 
 import java.util.UUID;
@@ -29,7 +32,8 @@ public class ServiceModificationSteps {
     @When("^I send a PUT request to the /service/id endpoint$")
     public void iSendAPUTRequestToTheServiceEndpoint() throws Throwable {
         try {
-            environment.setLastApiResponse(api.updateServiceWithHttpInfo(environment.getServiceUUID(), service));
+            ApiResponse apiResponse = api.updateServiceWithHttpInfo(environment.getServiceUUID(), service);
+            environment.setLastApiResponse(apiResponse);
             environment.setLastApiCallThrewException(false);
             environment.setLastApiException(null);
             environment.setLastStatusCode(environment.getLastApiResponse().getStatusCode());
@@ -38,6 +42,9 @@ public class ServiceModificationSteps {
             environment.setLastApiResponse(null);
             environment.setLastApiException(e);
             environment.setLastStatusCode(environment.getLastApiException().getCode());
+            Gson gson = new Gson();
+            ApiError error = gson.fromJson(e.getResponseBody(), ApiError.class);
+            String s = "a";
         }
     }
 
