c87897bbf4dcf2e781b9d4d81c4651dfcb8d5bb7
Oceanos1 <evondom@hotmail.com>
2018-01-16 20:39:10 +0100

    avancé mais toujours pas trouvé Le bug

--- service-status-server/src/main/java/io/lozzikit/servicestatus/entities/IncidentEntity.java
+++ service-status-server/src/main/java/io/lozzikit/servicestatus/entities/IncidentEntity.java
@@ -1,11 +1,11 @@
 package io.lozzikit.servicestatus.entities;
 
-import io.lozzikit.servicestatus.api.dto.IncidentUpdate;
+import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Set;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -20,8 +20,9 @@ public class IncidentEntity implements Serializable {
     @Column(name = "title")
     private String title;
 
-    @OneToMany(targetEntity = IncidentUpdateEntity.class, fetch = FetchType.LAZY, cascade = {CascadeType.ALL}, mappedBy = "incidentEntity")
-    private Set<IncidentUpdateEntity> incidentUpdates;
+    @OneToMany( mappedBy = "incidentEntity", targetEntity = IncidentUpdateEntity.class, fetch = FetchType.LAZY)
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    private List<IncidentUpdateEntity> incidentUpdates;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "service_id", nullable = true)
@@ -48,11 +49,11 @@ public class IncidentEntity implements Serializable {
         this.title = title;
     }
 
-    public Set<IncidentUpdateEntity> getIncidentUpdates() {
+    public List<IncidentUpdateEntity> getIncidentUpdates() {
         return incidentUpdates;
     }
 
-    public void setIncidentUpdates(Set<IncidentUpdateEntity> incidentUpdates) {
+    public void setIncidentUpdates(List<IncidentUpdateEntity> incidentUpdates) {
         this.incidentUpdates = incidentUpdates;
     }
 
